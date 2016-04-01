ac5505593aa9c8e745b9b56f0190c89dfb667b50
zanagi <kintoki.douji@gmail.com>
2016-04-01 10:58:34 +0300

    Initialize load content code

---
+++ js/loadContent.js
@@ -0,0 +1,38 @@
+var loadContent = function() {
+	var hash = window.location.hash.substring(1) || '/about';
+	var url = "content" + hash + ".html";
+	
+	$.ajax({url: url, 
+        beforeSend: function( xhr ) {
+          xhr.overrideMimeType( "text/html; charset=UTF-8" );
+        },
+        success: function(data, textStatus, jqXHR) {
+          $("#content").html(data);
+        }, 
+        complete: function() {
+          // TODO: Upon completion hide loading marker
+        }});
+};
+
+$(window).ready(function(){
+	// Set a timer (interval) for updating content, if event 'hashchange' does not exist
+	if (!('onhashchange' in window)) {
+		// Save old URL.
+		var oldHref = location.href;
+		setInterval(function() {
+		  var newHref = location.href;
+		  if (oldHref !== newHref) {
+			// Update URL and load content.
+		    oldHref = newHref;
+		    loadContent();
+		  }
+		}, 100);
+	    
+	    // Debug print
+	    console.log("No onhashchange event. Using timer for updating content.");
+	} else {
+	    $(window).on('hashchange', function() {
+	    	loadContent();
+	    });
+	}
+});
