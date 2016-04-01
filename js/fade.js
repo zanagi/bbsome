bbeb04418f13e670fb762028d38425f2aa7c7a9b
zanagi <kintoki.douji@gmail.com>
2016-04-01 10:57:18 +0300

    fadeout of title

--- js/fade.js
+++ js/fade.js
@@ -1,8 +1,14 @@
-$(document).ready(function() {
-	$("#front").fadeOut(5000, function() {
+$(window).ready(function() {
+	// Animation of the title fade-in and fade-out
+	$("#front").fadeOut(300, function() {
 		$("#title").animate({
 			opacity: 1.0,
 			top: "-=10%"
-		}, 3000);
+		}, 3000, function() {
+			$("#title").animate({
+				opacity: .0,
+				top: "-=10%"
+			}, 3000);
+		});
 	});
 });
