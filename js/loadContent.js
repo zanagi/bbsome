9ce9286c65bd032efdf338b793031e1d76994258
zanagi <kintoki.douji@gmail.com>
2016-04-11 11:12:12 +0300

    Content fade animation, content, tabs

--- js/loadContent.js
+++ js/loadContent.js
@@ -1,28 +1,82 @@
+var htmlData = [];
+var debug = true;
+
 var loadContent = function() {
-	var hash = window.location.hash.substring(1) || '/about';
+	var hash = window.location.hash.substring(1) || 'home';
 	var url = "content/" + hash + ".html";
 	
-	// Empty content
-	$("#content").empty();
+	var ajaxLoad = function() {
+		// Empty content 
+		$("#text-content").empty();
+		
+		// Load content using AJAX
+		$.ajax({url: url, 
+	        beforeSend: function(xhr) {
+	          xhr.overrideMimeType("text/html; charset=UTF-8");
+	        },
+	        success: function(data, textStatus, jqXHR) {
+	          $("#text-content").html(data);
+	          $("#text-content").fadeIn(1000);
+	          
+	          // Debug print
+	          if(debug) console.log("Data loaded from: " + url);
+	        }, 
+	        complete: function() {
+	          // TODO: Some load indicator?
+	        }});
+	};
 	
-	// Load content using AJAX
-	$.ajax({url: url, 
-        beforeSend: function(xhr) {
-          xhr.overrideMimeType("text/html; charset=UTF-8");
-        },
-        success: function(data, textStatus, jqXHR) {
-          $("#content").html(data);
-        }, 
-        complete: function() {
-          // TODO: Upon completion hide loading marker
-        }});
+	// Fade-out animation, after completion call load function
+	$("#text-content").fadeOut(1000, ajaxLoad);
+};
+
+var animate = function() {
+	$("#front").fadeOut(3000, function() {
+		// Title fade-in
+		$("#title").animate({
+			opacity: 1.0,
+			top: "-=10%"
+		}, 1500, function() {
+			// Title fade-out
+			$("#title").animate({
+				opacity: .0,
+				top: "-=10%"
+			}, 1500, function(){
+				// Content fade-in
+				$("#content").show();
+				$("#content").animate({
+					opacity: 1.0,
+				}, 2000);
+			});
+		});
+	});
+};
+
+var setTabListener = function() {
+	var showTabText = function(id) {
+		$(".active-tab-text").toggleClass("active-tab-text");
+		$("#" + id).toggleClass("active-tab-text");
+	};
+	
+	$("body").on("click", ".tab", function(){
+		$(".active-tab").toggleClass("active-tab");
+		$(this).toggleClass("active-tab");
+		var id = $(this).attr("id");
+		var textId = id + "-text";
+		showTabText(textId);
+	});
 };
 
 $(window).ready(function(){
-	// Set a timer (interval) for updating content, if event 'hashchange' does not exist
+	animate();
+	loadContent();
+	setTabListener();
+	
 	if (!('onhashchange' in window)) {
 		// Save old URL.
 		var oldHref = location.href;
+
+		// Set a timer (interval) for updating content, if event 'hashchange' does not exist
 		setInterval(function() {
 		  var newHref = location.href;
 		  if (oldHref !== newHref) {
@@ -33,7 +87,7 @@ $(window).ready(function(){
 		}, 100);
 	    
 	    // Debug print
-	    console.log('No "onhashchange" event. Using timer for updating content.');
+		if(debug) console.log('No "onhashchange" event. Using timer for updating content.');
 	} else {
 	    $(window).on('hashchange', function() {
 	    	loadContent();
